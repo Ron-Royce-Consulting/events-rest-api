965ce9202838264b7cb084327ae87f69919a11f4
Ron Royce <rroyce@ronroyce.tech>
2024-09-01 10:51:05 +0000

    gunicorn config changes

--- api/gunicorn_config.py
+++ api/gunicorn_config.py
@@ -1,42 +1,52 @@
-# gunicorn_config.py
+import os
+import socket
 
-# Server socket
+
 bind = "0.0.0.0:8080"
-backlog = 2048
-
-# Worker processes
-workers = 3
-worker_class = "uvicorn.workers.UvicornWorker"
-# worker_connections = 1000
-timeout = 30
-keepalive = 2
-
-# Server mechanics
-#daemon = False
-#pidfile = None
-#umask = 0
-#user = None
-#group = None
-#tmp_upload_dir = None
-
-# Logging
-errorlog = "-"
-loglevel = "info"
+workers = int(os.getenv("GUNICORN_WORKER_COUNT", str((2 * os.cpu_count() + 1))))
 accesslog = "-"
-access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+capture_output = True
+enable_stdio_inheritance = True
+level=os.getenv('LOG_LEVEL', 'ERROR').upper()
+hostname = socket.gethostname()
 
-# Process naming
-proc_name = None
+print(f"Workers: {workers}")
+print(f"Log Level: {level}")
 
-# Server hooks
-def on_starting(server):
-    pass
+# Define syslog format
+access_logformat = f'%(asctime)s {hostname} %(name)s[%(process)d]: %(message)s'
 
-def on_reload(server):
-    pass
-
-def when_ready(server):
-    pass
-
-def on_exit(server):
-    pass
+logconfig_dict = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': access_logformat
+        },
+        'access': {
+            'format': access_logformat
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default'
+        }
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO'
+    },
+    'loggers': {
+        'gunicorn.access': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False
+        },
+        'gunicorn.error': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False
+        }
+    }
+}
